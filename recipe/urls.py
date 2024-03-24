df909967bb5552efb45be4985edbe3f7680d35e8
Dionisie Dobroslav <dobroslav.denis@mail.ru>
2024-03-24 20:07:27 +0100

    CRUD

--- recipe/urls.py
+++ recipe/urls.py
@@ -19,5 +19,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name="homepage")
+    path('',views.RecipeListView.as_view(), name="homepage"),
+    path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name="detail"),
+    path('recipe/create', views.RecipeCreateView.as_view(), name="create"),
+    path('recipe/<int:pk>/update', views.RecipeUpdateView.as_view(), name="update"),
+    path('recipe/<int:pk>/delete', views.RecipeDeleteView.as_view(), name="delete"),
+    path('about/', views.about, name='about'),
 ]
